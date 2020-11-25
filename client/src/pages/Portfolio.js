0ff1b76761029e5ead5810e5c002347306f5d358
aidabyte <ablinstrubyte@gmail.com>
2020-11-24 19:37:34 -0600

    added container around cards in portfolio

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Container from "../components/Container"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from "../components/Card";
 
@@ -7,8 +7,10 @@ import Card from "../components/Card";
 function Portfolio() {
   return (
     <div>
+      <Container>
       <Card>
       </Card>
+      </Container>
      
       
       </div>

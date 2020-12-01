adcc2841519e4480281361bf81bcdf3f32959a0a
aidabyte <ablinstrubyte@gmail.com>
2020-11-30 22:39:16 -0600

    added buttons and links

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Container from "../components/Container";
+import SingleCard from "../components/SingleCard";
+import Footer from "../components/Footer";
+
 
 
 function Contact() {
     return (
-      <div>hello
+      <div>
+        <Container>
+        <SingleCard>
+      </SingleCard>
+        </Container>
        
-        
+        <Footer>  
+        </Footer>
         </div>
     );
   }

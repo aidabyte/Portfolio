88c3f644fad9a473bbe36a054009b0b02bc1b7c5
aidabyte <ablinstrubyte@gmail.com>
2020-11-30 22:41:02 -0600

    final changes

---
+++ client/src/components/SingleCard/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css"
+
+function SingleCard() {
+    return (
+        <div>
+            <div class="cardss mx-auto">
+                <h5 class="card-header">Contact</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Email: ablinstrubyte@gmail.com</h5>
+                    <p class="card-text">#: 630.854.3112</p>
+                    <h5 class="card-title"></h5>
+                    <a href="https://www.linkedin.com/in/aida-blinstrubyte-04a3091b6/" target="_blank"
+                class="btn btn-dark">LinkedIn</a>
+                    <a href="https://github.com/aidabyte" class="btn btn-dark" target="_blank">Github</a>
+                    <a href="./AidaResume.pdf" class="btn btn-dark" target="_blank">Resume</a>
+                </div>
+            </div>
+        </div>
+        
+    );
+}
+  
+ 
+
+export default SingleCard;

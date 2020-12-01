a25ac6ccb18d466d0a94a07ee05e78bba4b02767
aidabyte <ablinstrubyte@gmail.com>
2020-11-30 22:39:55 -0600

    styling the cards

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,22 +1,50 @@
 import React from "react";
+import "./style.css"
 
-function Card() {
+
+
+function Card(props) {
   return (
-    <div className="container">
-         <div class="row mb-3"> 
-    <div class="col-md-4">
-    </div>
-    <div class="col-md-2">
-    <div class="card cards">
-      <img class="card-img-top" src="Images/hiking.jpg"  />
-      <div class="card-body"> 
-      </div>
-    </div>
-  </div>
-  </div>
-</div>
+ <div>
+  <div className="row">
+    <div className="container-fluid">
+      <div className="col-sm-6">
+      
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title title">{props.data.title}</h5>
+              <img src={props.data.image} alt="project " />
+              <a href={props.data.link} target="_blank"className="btn btn-dark">Website</a>
+              <a href={props.data.gitLink} target="_blank" onClick="window.open{props.data.gitLink}"className="btn btn-dark">Gitub</a>
+              
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        
 
-    
+        
+        
+        
+         
+       
+        
+        
+
+        
+       
+        
+        
+       
+      
+      
+        
+      
+
+
+
   );
 }
 

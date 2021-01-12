02b4d448dad474d2eaaf68c28704edc71972f9cd
aidabyte <ablinstrubyte@gmail.com>
2021-01-11 20:48:18 -0600

    adjusted footer

--- client/src/components/SingleCard/index.js
+++ client/src/components/SingleCard/index.js
@@ -8,12 +8,12 @@ function SingleCard() {
                 <h5 class="card-header">Contact</h5>
                 <div class="card-body">
                     <h5 class="card-title">Email: ablinstrubyte@gmail.com</h5>
-                    <p class="card-text">#: 630.854.3112</p>
+                   
                     <h5 class="card-title"></h5>
                     <a href="https://www.linkedin.com/in/aida-blinstrubyte-04a3091b6/" target="_blank"
                 class="btn btn-dark">LinkedIn</a>
                     <a href="https://github.com/aidabyte" class="btn btn-dark" target="_blank">Github</a>
-                    <a href="./AidaResume.pdf" class="btn btn-dark" target="_blank">Resume</a>
+                    <a href="./Resume.pdf" class="btn btn-dark" target="_blank">Resume</a>
                 </div>
             </div>
         </div>

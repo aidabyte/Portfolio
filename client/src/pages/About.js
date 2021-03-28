58ec875e21a963167e9d30a1620818db7592ab6f
aidabyte <ablinstrubyte@gmail.com>
2021-03-28 10:02:47 -0500

    added a space in paragraph

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -22,7 +22,7 @@ function About() {
         <Row>
           <Col size="md-12">
             <p>
-            Full Stack web developer primarily focused in front end web development with a background in Cosmetology. Certificate in Full Stack Web Development from Northwestern University. Newly developed skills in HTML, CSS, JavaScript, MongoDB, APIs, MySQL, Node.js, React, and responsive web design. Strengths in teamwork, communication, and collaboration. Known for having a great eye in design and enthusiasm for developing apps that are mobile friendly. Every project goal is to have the user have the best experience possible and everything to be properly functioning. Excited to utilize strong skills such as critical thinking and evaluating results to choose the best solution and solve problems that occur, along with my technical skills, and a meticulous eye for detail making me an asset to any driven team. 
+            Full Stack web developer primarily focused in front end web development with a background in Cosmetology. Certificate in Full Stack Web Development from Northwestern University. Newly developed skills in HTML, CSS, JavaScript, MongoDB, APIs, MySQL, Node.js, React, and responsive web design. Strengths in teamwork, communication, collaboration, and known for having a great eye in design and enthusiasm for developing apps that are mobile friendly. Every project goal is to have the user have the best experience possible and everything to be properly functioning. Excited to utilize strong skills such as critical thinking and evaluating results to choose the best solution and solve problems that occur, along with my technical skills, and a meticulous eye for detail making me an asset to any driven team. 
 </p>
             <p>
               

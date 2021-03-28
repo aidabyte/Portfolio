af4e47ec746548fdc3eafbbe74ae8bfe14041c50
aidabyte <ablinstrubyte@gmail.com>
2021-03-28 09:55:45 -0500

    edited paragraph about me

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -22,7 +22,7 @@ function About() {
         <Row>
           <Col size="md-12">
             <p>
-            Full Stack web developer with a background in Cosmetology. Certificate in Full Stack Web Development from Northwestern University in Chicago. Newly developed skills in HTML, CSS, JavaScript, MongoDB, APIs, MySQL, Node.js, React, and responsive web design. Strengths in teamwork, communication, and collaboration.Known for having a great eye in design and enthusiasm for developing apps that are mobile friendly. With every project my goal is to have the user have the best experience possible and everything to be properly functioning. I worked on a team of four towards a React app and my main focus was the design and functionality. I’m excited to express my experience from my cosmetology background, joined together with my technical skills, and a meticulous eye for detail making me an asset to any driven team. 
+            Full Stack web developer primarily focused in front end web development with a background in Cosmetology. Certificate in Full Stack Web Development from Northwestern University. Newly developed skills in HTML, CSS, JavaScript, MongoDB, APIs, MySQL, Node.js, React, and responsive web design. Strengths in teamwork, communication, and collaboration. Known for having a great eye in design and enthusiasm for developing apps that are mobile friendly. Every project goal is to have the user have the best experience possible and everything to be properly functioning. Excited to utilize strong skills such as critical thinking and evaluating results to choose the best solution and solve problems that occur, along with my technical skills, and a meticulous eye for detail making me an asset to any driven team. 
 </p>
             <p>
               

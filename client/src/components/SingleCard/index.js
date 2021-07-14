aa8903683de62827d66be43a6b6249429bfad1dd
aidabyte <ablinstrubyte@gmail.com>
2021-07-14 13:58:42 -0500

    added email button and icon

--- client/src/components/SingleCard/index.js
+++ client/src/components/SingleCard/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./style.css";
 import {FaGithub,FaLinkedin} from "react-icons/fa";
+import {MdEmail} from "react-icons/md";
+
 
 
 function SingleCard() {
@@ -9,14 +11,12 @@ function SingleCard() {
             <div class="cardss">
                 <h5 class="card-header">Contact</h5>
                 <div class="card-body">
-                    <h5 class="card-title">Email: a.blinstrubyte@outlook.com</h5>
-                   
-                    <h5 class="card-title"></h5>
+                    <a href="mailto: a.blinstrubyte@outlook.com" target="_blank" class="btn btn-dark">Email <MdEmail/></a>
+                    
                     <a href="https://www.linkedin.com/in/aida-blinstrubyte-04a3091b6/" target="_blank"
                 class="btn btn-dark">LinkedIn <FaLinkedin/><i class="fab fa-linkedin-in"></i></a>
         
-                <FaGithub/>
-                    <a href="https://github.com/aidabyte" class="btn btn-dark" target="_blank">Github</a>
+                    <a href="https://github.com/aidabyte" class="btn btn-dark" target="_blank">Github <FaGithub/></a> 
                     <a href="./FSF_RESUME.pdf" class="btn btn-dark" target="_blank">Resume</a>
                 </div>
             </div>

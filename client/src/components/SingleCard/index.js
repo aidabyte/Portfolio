66bd3663e272c2066dac7c76605ad994788f5e18
aidabyte <ablinstrubyte@gmail.com>
2021-07-14 14:27:37 -0500

    added all of the icons

--- client/src/components/SingleCard/index.js
+++ client/src/components/SingleCard/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import {FaGithub,FaLinkedin} from "react-icons/fa";
 import {MdEmail} from "react-icons/md";
+import {FiFileText} from "react-icons/fi";
 
 
 
@@ -9,15 +10,15 @@ function SingleCard() {
     return (
         <div>
             <div class="cardss">
-                <h5 class="card-header">Contact</h5>
+                {/* <h5 class="card-header">Contact</h5> */}
                 <div class="card-body">
-                    <a href="mailto: a.blinstrubyte@outlook.com" target="_blank" class="btn btn-dark">Email <MdEmail/></a>
+                    <a href="mailto: a.blinstrubyte@outlook.com" target="_blank" class="btn btn-dark btnn">Email <MdEmail/></a>
                     
                     <a href="https://www.linkedin.com/in/aida-blinstrubyte-04a3091b6/" target="_blank"
-                class="btn btn-dark">LinkedIn <FaLinkedin/><i class="fab fa-linkedin-in"></i></a>
+                class="btn btn-dark btnn">LinkedIn <FaLinkedin/><i class="fab fa-linkedin-in"></i></a>
         
-                    <a href="https://github.com/aidabyte" class="btn btn-dark" target="_blank">Github <FaGithub/></a> 
-                    <a href="./FSF_RESUME.pdf" class="btn btn-dark" target="_blank">Resume</a>
+                    <a href="https://github.com/aidabyte" class="btn btn-dark btnn" target="_blank">Github <FaGithub/></a> 
+                    <a href="./FSF_RESUME.pdf" class="btn btn-dark btnn" target="_blank">Resume <FiFileText/></a>
                 </div>
             </div>
         </div>

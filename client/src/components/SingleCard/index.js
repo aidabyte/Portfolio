32a6bc7a3dd3f2beff8ba880df547896e3adfd0d
aidabyte <ablinstrubyte@gmail.com>
2021-07-14 13:43:44 -0500

    added linkedin icon

--- client/src/components/SingleCard/index.js
+++ client/src/components/SingleCard/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
+import {FaGithub,FaLinkedin} from "react-icons/fa";
+
 
 function SingleCard() {
     return (
@@ -11,7 +13,9 @@ function SingleCard() {
                    
                     <h5 class="card-title"></h5>
                     <a href="https://www.linkedin.com/in/aida-blinstrubyte-04a3091b6/" target="_blank"
-                class="btn btn-dark">LinkedIn</a>
+                class="btn btn-dark">LinkedIn <FaLinkedin/><i class="fab fa-linkedin-in"></i></a>
+        
+                <FaGithub/>
                     <a href="https://github.com/aidabyte" class="btn btn-dark" target="_blank">Github</a>
                     <a href="./FSF_RESUME.pdf" class="btn btn-dark" target="_blank">Resume</a>
                 </div>
